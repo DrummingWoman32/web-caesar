1d72e4aa3e2d8813a453ea23cbe59650832cf26d
M B-Triff <mcb_triff@yahoo.com>
2018-09-21 15:24:47 -0400

    Did up to 'importing caesar code'

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """ 
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <!-- create your form here-->
+
+        <!-- POST method to be used -->
+
+        Rotate by: <input type="text" name="rot" value="0">
+        <br>
+        <br>
+        <textarea name="text"></textarea> 
+        <br>
+        <input type="submit" value="Submit Query">
+
+
+
+    </body>
+</html>
+
+"""
+
+
+@app.route("/")
+def index():
+    return form
+
+app.run()
+
+#left off on "Importing caesar code"
