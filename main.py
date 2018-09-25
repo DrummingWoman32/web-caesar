5e6b50b9bdda3e005aaa6861242716efb3a3217e
M B-Triff <mcb_triff@yahoo.com>
2018-09-25 16:34:18 -0400

    Finish web caesar

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request
 from caesar import rotate_string
 
@@ -10,53 +11,60 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
+            }}
 
-            textarea {
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <!-- create your form here-->
 
-        <!-- POST method to be used -->
-
-        Rotate by: <input type="text" name="rot" value="0">
-        <br>
-        <br>
-        <textarea name="text"></textarea> 
-        <br>
-        <input type="submit" value="Submit Query">
-        <br>
-        <br>
+        <form action="/" method="post">
+            <label for="to_encrypt">Rotate by: </label>
+            <input type="text" name="rot" value="0" id="to_encrypt">
+            <br>
+            <br>
+            <textarea name="text">{0}</textarea> 
+            <br>
+            <input type="submit" value="Submit Query">
+            <br>
+            <br>
+        </form>
  
     </body>
 </html>
 
 """
+
+#action="/hello" method="post">
+
+#{0} above is placeholder for encrypted text
+#so here I encrypt the text and return it
 @app.route("/", methods=['POST'])
-def encrypt(rot, text):
+def encrypt():
 
-    #encrypt_form = request.form['rot']
-    form += ("<h1>" + str(rotate_string(text, rot)) + "</h1>")
-    return form
-
-
-
+    rot = int(request.form['rot'])
+    #rot must be typecast into integer it actually is
+    text = request.form['text']
+    encrypted_text = rotate_string(text, rot)
+    return form.format(encrypted_text)
+    
 
 @app.route("/")
 def index():
-    return form
+
+    return form.format("")
 
 app.run()
 
